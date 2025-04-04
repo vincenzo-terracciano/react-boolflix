e27f9507745ae646a07ef8eba23263dd40e90c97
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-04-04 18:24:42 +0200

    Add flag display for movie languages and update styles

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,6 +4,30 @@ export default function Movies() {
 
     const { movies } = useGlobalContext()
 
+    function flag(language) {
+        if (!language) return null;
+
+        if (language === 'en') {
+            language = 'gb'
+        } else if (language === 'ja') {
+            language = 'jp'
+        } else if (language === 'zh') {
+            language = 'ch'
+        } else if (language === 'it') {
+            language = 'it'
+        } else if (language === 'es') {
+            language = 'es'
+        } else {
+            return null
+        }
+
+
+
+        const flagUrl = `https://flagpedia.net/data/flags/h80/${language}.png`
+
+        return flagUrl
+    }
+
     return (
         <>
             <div className="container mt-5">
@@ -12,10 +36,20 @@ export default function Movies() {
                         {
                             movies.map(movie => (
                                 <li key={`Movie - ${movie.id}`}>
-                                    <h6>{movie.title}</h6>
-                                    <h6>{movie.original_title}</h6>
-                                    <h6>{movie.original_language}</h6>
-                                    <h6>{movie.vote_average}</h6>
+                                    <h6>Title: {movie.title}</h6>
+                                    <h6>Original Title: {movie.original_title}</h6>
+                                    <div className="flag">
+                                        <strong>Language: </strong>
+
+                                        {flag(movie.original_language) ? (
+                                            <img src={flag(movie.original_language)} alt={movie.original_language} />
+                                        ) : (
+                                            <span>{movie.original_language.toUpperCase()}</span>
+                                        )}
+
+                                    </div>
+
+                                    <h6>Vote: {movie.vote_average}</h6>
                                 </li>
                             ))
                         }
