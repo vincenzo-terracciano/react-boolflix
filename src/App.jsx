1ebd9019570429d0c07d9c348e0f0fdfcc27b82b
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-04-04 12:37:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { MoviesProvider } from "./contexts/GlobalContext"
+
+
+function App() {
+
+
+  return (
+    <>
+      <MoviesProvider>
+
+      </MoviesProvider>
+    </>
+  )
+}
+
+export default App
