f092495168d1554a072f0af25fc67e7626668f6d
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-04-07 16:31:20 +0200

    enhance styles for better visibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,13 @@ export default function Header() {
                     <div className="collapse navbar-collapse" id="collapsibleNavId">
                         <ul className="navbar-nav me-auto mt-2 mt-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link text-white" href="/">Home</a>
+                                <a className="nav-link" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-white" href="/chisiamo">Movies</a>
+                                <a className="nav-link" href="#">Movies</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-white" href="/posts">TV Series</a>
+                                <a className="nav-link" href="#">TV Series</a>
                             </li>
                         </ul>
                     </div>
